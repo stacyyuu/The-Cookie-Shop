64f534cdb6c54826fc6f3d1bf609ff26f3c26eec
Stacy Yu <stacyyuu@gmail.com>
2022-06-25 15:32:07 -0700

    updates to cookie store

---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict'
+// use strict tell us to follow some baseline 'strict rules'
+// variables = storing data as values. declore some sort of data and add value to it 
+// declaring variable firstname to equal value of stacy 
+// let firstName = "stacy";
+
+// let firstName = prompt("What is your name?");
+
+// if(firstName === "Stacy"){
+//     console.log("Your name is Stacy!")
+// }
+
+let firstName = prompt("What is your name?");
+let time = prompt("What hour is it (0-24)?");
+let background = prompt("Change background color to:");
+let message = "";
+
+if(time <= 11){
+    message = "Good Morning!";
+    //console.log("Good Morning!");
+} else if(time <= 18){
+    message = "Good Afternoon!";
+    //console.log("Good Afternoon!");
+} else if(time <= 24){
+    message = "Good Night!";
+    //console.log("Good Night!");
+} else{
+    message = "Good Day!";
+    //console.log("Have a great day!");
+}
+if (confirm("Confirm Answers?") == true) {
+    document.write("Hello " + firstName + "! " + message);
+    document.body.style.backgroundColor("background");
+}
